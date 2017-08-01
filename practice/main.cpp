1becf2ce803a91102b0940f6a243461a3a9ac7a6
程潇 <769478099@qq.com>
2017-08-01 13:53:55 +0800

    helloworld

---
+++ practice/main.cpp
@@ -0,0 +1,20 @@
+//
+//  main.cpp
+//  practice
+//
+//  Created by 程潇 on 2017/7/17.
+//  Copyright © 2017年 程潇. All rights reserved.
+//
+
+#include <iostream>
+#include <vector>
+#include <list>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    list<int> ilist;
+    for (size_t ix = 0; ix !=4; ++ix)
+        ilist.push_front(ix);
+    
+}
